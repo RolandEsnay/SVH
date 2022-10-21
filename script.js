15e2941af88fdb89750a11a599e0a5f0a5a0d995
ESNAY Roland (SNCF VOYAGEURS / Direction de Lignes TER Est Provence Alpes / SIEGE UO RC TOULON) <7807212a@commun.ad.sncf.fr>
2022-10-21 14:57:40 +0200

    add some fix

--- script.js
+++ script.js
@@ -1,51 +1,52 @@
-let img__slider = document.getElementsByClassName('img__slider');
+let img__slider = document.getElementsByClassName("img__slider");
 
 let etape = 0;
 
 let nbr__img = img__slider.length;
 
-let precedent = document.querySelector('.precedent');
-let suivant = document.querySelector('.suivant');
+let precedent = document.querySelector(".precedent");
+let suivant = document.querySelector(".suivant");
 
 function enleverActiveImages() {
-    for(let i = 0 ; i < nbr__img ; i++) {
-        img__slider[i].classList.remove('active');
-    }
+  for (let i = 0; i < nbr__img; i++) {
+    img__slider[i].classList.remove("active");
+  }
 }
 
-suivant.addEventListener('click', function() {
-    etape++;
-    if(etape >= nbr__img) {
-        etape = 0;
-    }
-    enleverActiveImages();
-    img__slider[etape].classList.add('active');
-})
+suivant.addEventListener("click", function () {
+  etape++;
+  if (etape >= nbr__img) {
+    etape = 0;
+  }
+  enleverActiveImages();
+  img__slider[etape].classList.add("active");
+});
 
-precedent.addEventListener('click', function() {
-    etape--;
-    if(etape < 0) {
-        etape = nbr__img - 1;
-    }
-    enleverActiveImages();
-    img__slider[etape].classList.add('active');
-})
+precedent.addEventListener("click", function () {
+  etape--;
+  if (etape < 0) {
+    etape = nbr__img - 1;
+  }
+  enleverActiveImages();
+  img__slider[etape].classList.add("active");
+});
 
-setInterval(function() {
-    etape++;
-    if(etape >= nbr__img) {
-        etape = 0;
-    }
-    enleverActiveImages();
-    img__slider[etape].classList.add('active');
-}, 5000)
-
+setInterval(function () {
+  etape++;
+  if (etape >= nbr__img) {
+    etape = 0;
+  }
+  enleverActiveImages();
+  img__slider[etape].classList.add("active");
+}, 5000);
 
 // MENU BURGER
 
-const menuHamburger = document.querySelector(".menu-burger")
-        const navLinks = document.querySelector(".nav-list")
- 
-        menuHamburger.addEventListener('click',()=>{
-        navLinks.classList.toggle('mobile-menu')
-        })
+const menuHamburger = document.querySelector(".menu-burger");
+const navLinks = document.querySelector(".nav-list");
+
+menuHamburger.addEventListener("click", () => {
+  navLinks.classList.toggle("mobile-menu");
+});
+
+//DROPDOWN MOBILE MENU
