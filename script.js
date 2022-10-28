e22a56922844f096ee96d47dc2a565ca98c01524
ESNAY Roland (SNCF VOYAGEURS / Direction de Lignes TER Est Provence Alpes / SIEGE UO RC TOULON) <7807212a@commun.ad.sncf.fr>
2022-10-28 10:19:11 +0200

    modif url img pour github pages

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-
 //  SLIDER //
 const slider = document.querySelector(".slider");
 const nextBtn = document.querySelector(".next-btn");
@@ -9,25 +8,24 @@ const numberOfSlides = slides.length;
 var slideNumber = 0;
 
 // image slider next button
-nextBtn.addEventListener("click",()=>{
-  slides.forEach((slide)=>{
-    slide.classList.remove("active")
+nextBtn.addEventListener("click", () => {
+  slides.forEach((slide) => {
+    slide.classList.remove("active");
   });
-  slideIcons.forEach((slideIcon)=>{
-    slideIcon.classList.remove("active")
+  slideIcons.forEach((slideIcon) => {
+    slideIcon.classList.remove("active");
   });
 
-  slideNumber ++;
+  slideNumber++;
 
-  if(slideNumber > (numberOfSlides - 1)){
-    slideNumber = 0
+  if (slideNumber > numberOfSlides - 1) {
+    slideNumber = 0;
   }
-  slides[slideNumber].classList.add("active")
-  slideIcons[slideNumber].classList.add("active")
-
-})
- //image slider previous button
- prevBtn.addEventListener("click", () => {
+  slides[slideNumber].classList.add("active");
+  slideIcons[slideNumber].classList.add("active");
+});
+//image slider previous button
+prevBtn.addEventListener("click", () => {
   slides.forEach((slide) => {
     slide.classList.remove("active");
   });
@@ -37,7 +35,7 @@ nextBtn.addEventListener("click",()=>{
 
   slideNumber--;
 
-  if(slideNumber < 0){
+  if (slideNumber < 0) {
     slideNumber = numberOfSlides - 1;
   }
 
@@ -48,7 +46,7 @@ nextBtn.addEventListener("click",()=>{
 var playSlider;
 
 var repeater = () => {
-  playSlider = setInterval(function(){
+  playSlider = setInterval(function () {
     slides.forEach((slide) => {
       slide.classList.remove("active");
     });
@@ -58,77 +56,72 @@ var repeater = () => {
 
     slideNumber++;
 
-    if(slideNumber > (numberOfSlides - 1)){
+    if (slideNumber > numberOfSlides - 1) {
       slideNumber = 0;
     }
 
     slides[slideNumber].classList.add("active");
     slideIcons[slideNumber].classList.add("active");
   }, 4000);
-}
+};
 repeater();
 
-
-//SEARCH BUTTON 
+//SEARCH BUTTON
 //<i class="fa fa-times" aria-hidden="true"></i>
 
-let navBar = document.querySelector(".navbar")
-let searchBox = document.querySelector(".search-box .fa-search")
- 
-searchBox.addEventListener ("click", ()=>{
-  navBar.classList.toggle("showInput");
-  if (navBar.classList.contains("showInput")){
-    searchBox.classList.replace("fa-search","fa-times")
-  } else{
-  searchBox.classList.replace("fa-times","fa-search")}
-})
+let navBar = document.querySelector(".navbar");
+let searchBox = document.querySelector(".search-box .fa-search");
 
+searchBox.addEventListener("click", () => {
+  navBar.classList.toggle("showInput");
+  if (navBar.classList.contains("showInput")) {
+    searchBox.classList.replace("fa-search", "fa-times");
+  } else {
+    searchBox.classList.replace("fa-times", "fa-search");
+  }
+});
 
 // sidebar open close JS
 let navLinks = document.querySelector(".navbar .nav-links");
 let menuOpenBtn = document.querySelector(".navbar .fa-bars");
 let menuCloseBtn = document.querySelector(".nav-links .fa-times");
 
-menuOpenBtn.addEventListener("click", ()=> {
-navLinks.style.left= "0";
-
+menuOpenBtn.addEventListener("click", () => {
+  navLinks.style.left = "0";
 });
-menuCloseBtn.addEventListener("click", ()=> {
-  navLinks.style.left= "-100%";
-  
-  });
-  
-//SCRIPT SUB MENU MOBILE OPEN 
-let equipeArrow = document.querySelector (".equipe-arrow");
+menuCloseBtn.addEventListener("click", () => {
+  navLinks.style.left = "-100%";
+});
 
-equipeArrow.addEventListener("click", ()=>{
-navLinks.classList.toggle("show1");
-})
-let clubArrow = document.querySelector (".club-arrow");
+//SCRIPT SUB MENU MOBILE OPEN
+let equipeArrow = document.querySelector(".equipe-arrow");
 
-clubArrow.addEventListener("click", ()=>{
-navLinks.classList.toggle("show2");
-})
-let mediaArrow = document.querySelector (".media-arrow");
+equipeArrow.addEventListener("click", () => {
+  navLinks.classList.toggle("show1");
+});
+let clubArrow = document.querySelector(".club-arrow");
 
-mediaArrow.addEventListener("click", ()=>{
-navLinks.classList.toggle("show3");
-})
+clubArrow.addEventListener("click", () => {
+  navLinks.classList.toggle("show2");
+});
+let mediaArrow = document.querySelector(".media-arrow");
+
+mediaArrow.addEventListener("click", () => {
+  navLinks.classList.toggle("show3");
+});
 
 // Fixed navbar
 
 let navbar = document.getElementById("nav-bar");
-let mainNav = document.getElementById("main-nav")
+let mainNav = document.getElementById("main-nav");
 let navPos = navbar.getBoundingClientRect().top;
 
-window.addEventListener("scroll", e => {
+window.addEventListener("scroll", (e) => {
   let viewportHeight = window.innerHeight;
   let scrollPos = window.scrollY;
   if (scrollPos > navPos) {
-    mainNav.classList.add('sticky');
-
+    mainNav.classList.add("sticky");
   } else {
-   mainNav.classList.remove('sticky');
-
+    mainNav.classList.remove("sticky");
   }
 });
